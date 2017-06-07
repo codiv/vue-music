1b7a65940380b8ad85f117ebf986694a77718aa5
codiv <codiv@sina.com>
2017-06-07 14:55:33 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="app">
-
-  </div>
+	<div id="app">
+		<m-header></m-header>
+		<tab></tab>
+		<router-view></router-view>
+	</div>
 </template>
 
-<script>
-export default {
+<script type="text/ecmascript-6">
+	import MHeader from 'components/m-header/m-header'
+	import Tab from 'components/tab/tab'
 
-}
+	export default {
+		components: {
+			MHeader,
+			Tab
+		}
+	}
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+
+/* 解决移动端点击300毫秒的问题 */
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
