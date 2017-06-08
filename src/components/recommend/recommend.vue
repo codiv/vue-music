0cbb1609016e84435c2371eeb4e281aba9b97afa
codiv <codiv@sina.com>
2017-06-08 14:56:51 +0800

    修改

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,13 @@
 <template>
 	<div class="recommend">
-		<div v-for="item in recommends">
-			<img :src="item.picUrl">
+		<div class="slider-wrapper" v-if="recommends.length">
+			<slider>
+				<div v-for="item in recommends">
+					<a :href="item.linkUrl">
+						<img :src="item.picUrl">
+					</a>
+				</div>
+			</slider>
 		</div>
 	</div>
 
@@ -10,6 +16,7 @@
 <script type="text/ecmascript-6">
 	import {getRecommend} from 'api/recommend'
 	import {ERR_OK} from 'api/config'
+	import Slider from 'base/slider/slider'
 
 	export default {
 		data () {
@@ -28,6 +35,9 @@
 					}
 				})
 			}
+		},
+		components: {
+			Slider
 		}
 	}
 
@@ -35,5 +45,51 @@
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 
+	.recommend
+		position: fixed
+		width: 100%
+		top: 88px
+		bottom: 0
+		.recommend-content
+			height: 100%
+			overflow: hidden
+			.slider-wrapper
+				position: relative
+				width: 100%
+				overflow: hidden
+			.recommend-list
+				.list-title
+					height: 65px
+					line-height: 65px
+					text-align: center
+					font-size: $font-size-medium
+					color: $color-theme
+				.item
+					display: flex
+					box-sizing: border-box
+					align-items: center
+					padding: 0 20px 20px 20px
+					.icon
+						flex: 0 0 60px
+						width: 60px
+						padding-right: 20px
+					.text
+						display: flex
+						flex-direction: column
+						justify-content: center
+						flex: 1
+						line-height: 20px
+						overflow: hidden
+						font-size: $font-size-medium
+						.name
+							margin-bottom: 10px
+							color: $color-text
+						.desc
+							color: $color-text-d
+			.loading-container
+				position: absolute
+				width: 100%
+				top: 50%
+				transform: translateY(-50%)
 
 </style>
