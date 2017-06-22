8f14837e430d64c1701813325067b1cc7e4fb0b3
codiv <codiv@sina.com>
2017-06-22 09:25:11 +0800

    music_list

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,13 +1,44 @@
 <template>
 	<div class="music-list">
-
+		<div class="back" @click="back">
+			<i class="icon-back"></i>
+		</div>
+		<h1 class="title" v-html="title"></h1>
+		<div class="bg-images" :style="bgStyle"></div>
 	</div>
-
 </template>
 
 <script type="text/ecmascript-6">
 	
-	export default {}
+	export default {
+		props: {
+			songs: {
+				type: Array,
+				default: []
+			},
+			bgImage: {
+				type: String,
+				default: ''
+			},
+			title: {
+				type: String,
+				default: ''
+			}
+		},
+		methods: {
+			back() {
+				this.$router.back()
+			}
+		},
+		created() {
+			console.log(this.bgImage)
+		},
+		computed: {
+			bgStyle() {
+				return `background-image:url(${this.bgImage})`
+			}
+		}
+	}
 
 </script>
 
@@ -22,5 +53,32 @@
 		bottom: 0
 		right: 0
 		background: $color-background
-
+		.back
+			position: absolute
+			z-index: 50
+			top: 0
+			left: 6px
+			.icon-back
+				display: block
+				padding: 10px
+				font-size: $font-size-large-x
+				color: $color-theme
+		.title
+			position: absolute
+			top: 0
+			left: 10%
+			z-index: 40
+			width: 80%
+			no-wrap()
+			text-align: center
+			line-height: 40px
+			font-size: $font-size-large
+			color: $color-text
+		.bg-images
+			position: relative
+			width: 100%
+			height: 0
+			padding-top: 70%
+			transform-origin: top
+			background-size: cover
 </style>
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,33 +1,68 @@
 <template>
 	<transition name="slide">
-		<music-list></music-list>
+		<music-list :songs="songs" :title="title" :bgImage="bgImage"></music-list>
 	</transition>
 </template>
 
 <script type="text/ecmascript-6">
 	import MusicList from 'components/music-list/music-list'
 	import {mapGetters} from 'vuex'
-	
+	import {getSingerDetail} from 'api/singer'
+	import {createSong} from 'common/js/songs'
+	import {ERR_OK} from 'api/config'
+
 	export default {
 		computed: {
+			title() {
+				return this.singer.name
+			},
+			bgImage() {
+				return this.singer.avatar
+			},
 			...mapGetters([
 				'singer'
 			])
 		},
+		data() {
+			return {
+				songs: []
+			}
+		},
 		created() {
-			console.log(this.singer)
+			this._getDetail()
+		},
+		methods: {
+			_getDetail() {
+				if (!this.singer.id) {
+					this.$router.push('/singer')
+					return
+				}
+				getSingerDetail(this.singer.id).then((res) => {
+					if (res.code === ERR_OK) {
+						this.songs = this._normalizeSongs(res.data.list)
+					}
+				})
+			},
+			_normalizeSongs(list) {
+				let ret = []
+				list.forEach((item) => {
+					let {musicData} = item //取得item中的每一项musicData数据
+					if (musicData.songid && musicData.albummid) {
+						ret.push(createSong(musicData))
+					}
+				})
+				return ret
+			}
 		},
 		components: {
 			MusicList
 		}
 	}
-
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 	.slide-enter-active, .slide-leave-active
-		transition: all 0.4s
+		transition: all 0.3s
 	.slide-enter, .slide-leave-to
 		transform: translate3d(100%, 0, 0)
-
 </style>
