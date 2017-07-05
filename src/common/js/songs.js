6da74f340acf4d0c72ba8cb6f71901fbc141e666
codiv <codiv@sina.com>
2017-07-05 17:38:49 +0800

    lyric_config_optimize
    歌词配置的性能优化

--- src/common/js/songs.js
+++ src/common/js/songs.js
@@ -18,30 +18,41 @@ export default class Song {
     }
 
     getLyric() {
-        getLyric(this.mid).then((res) => {
-            /*
-             *把类似于JSONPCallback的数据进行处理
-             * 数据（res）：
-             * "MusicJsonCallback({\"retcode\":0,\"code\":0,\"subcode\":0,\"lyric\":\"})"
-             *对res进行处理，这个处理可以放在dev-sever.js里进行
-             * 做法如下：
-             * 1.先用正则表达处理，进行分组，得出“matches”
-             * 2.取出所需的数据，得出matches[1]
-             * 3.对数据（matches[1]） JSON字符串进行解析
-             *
-             *  */
-            if (typeof res === 'string') {
-                let reg = /^\w+\(({[^()]+})\)/
-                let matches = res.match(reg)
-                if (matches) {
-                    console.log(matches)
-                    res = JSON.parse(matches[1]) //JSON.parse()方法解析一个JSON字符串
+        /*
+         * 性能优化：为了避免都请求歌词，进行优化
+         * （代码对比请查看github中的26_playing_lyric_config分支里的两个节点）
+         * */
+        if (this.lyric) {
+            return Promise.resolve(this.lyric)
+        }
+
+        return new Promise((resolve, reject) => {
+            getLyric(this.mid).then((res) => {
+                /*
+                 *把类似于JSONPCallback的数据进行处理
+                 * 数据（res）：
+                 * "MusicJsonCallback({\"retcode\":0,\"code\":0,\"subcode\":0,\"lyric\":\"})"
+                 *对res进行处理，这个处理可以放在dev-sever.js里进行
+                 * 做法如下：
+                 * 1.先用正则表达处理，进行分组，得出“matches”
+                 * 2.取出所需的数据，得出matches[1]
+                 * 3.对数据（matches[1]） JSON字符串进行解析
+                 *
+                 *  */
+                if (typeof res === 'string') {
+                    let reg = /^\w+\(({[^()]+})\)/
+                    let matches = res.match(reg)
+                    if (matches) {
+                        res = JSON.parse(matches[1]) //JSON.parse()方法解析一个JSON字符串
+                    }
                 }
-            }
-            if (res.retcode === ERR_OK) {
-                this.lyric = Base64.decode(res.lyric)
-                console.log(this.lyric)
-            }
+                if (res.retcode === ERR_OK) {
+                    this.lyric = Base64.decode(res.lyric)
+                    resolve(this.lyric)
+                } else {
+                    reject('no lyric') //没有歌词
+                }
+            })
         })
     }
 }
