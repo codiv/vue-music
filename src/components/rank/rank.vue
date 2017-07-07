744c0346f1c4477f257e78a25d1a5e312581e1bb
codiv <codiv@sina.com>
2017-07-07 17:35:30 +0800

    toplist_data_finish
    完成toplist页面music-list的数据调用

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -56,7 +56,6 @@
 				getTopList().then((res) => {
 					if (res.code === ERR_OK) {
 						this.topList = res.data.topList
-						console.log(this.topList)
 					}
 				})
 			},
--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -1,6 +1,6 @@
 <template>
 	<transition name="toplist">
-		<music-list></music-list>
+		<music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
 	</transition>
 
 </template>
@@ -8,9 +8,49 @@
 <script type="text/ecmascript-6">
 	import MusicList from 'components/music-list/music-list'
 	import {mapGetters} from 'vuex'
+	import {getMusicList} from 'api/rank'
+	import {ERR_OK} from 'api/config'
+	import {createSong} from 'common/js/songs'
 
 	export default {
+		data() {
+			return {
+				songs: []
+			}
+		},
+		created() {
+			this._getMusicList()
+		},
+		methods: {
+			_getMusicList() {
+				if (!this.toplist.id) {
+					this.$router.push('/rank')
+					return
+				}
+				getMusicList(this.toplist.id).then((res) => {
+					if (res.code === ERR_OK) {
+						this.songs = this._normalizeSongs(res.songlist)
+					}
+				})
+			},
+			_normalizeSongs(list) {
+				let ret = []
+				list.forEach((item) => {
+					const musicData = item.data
+					if (musicData.songid && musicData.albummid) {
+						ret.push(createSong(musicData))
+					}
+				})
+				return ret
+			}
+		},
 		computed: {
+			title() {
+				return this.toplist.topTitle
+			},
+			bgImage() {
+				return this.songs.length ? this.songs[0].image : '' //取第一首歌的图片做为封面
+			},
 			...mapGetters([
 				'toplist'
 			])
