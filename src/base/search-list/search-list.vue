5f0b22fecfb41dd3c6210c730abae1a252b799e0
codiv <codiv@sina.com>
2017-07-23 21:32:29 +0800

    创建动画

--- src/base/search-list/search-list.vue
+++ src/base/search-list/search-list.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="search-list" v-show="searches.length">
-		<ul>
-			<li class="search-item" v-for="item in searches" @click="selectItem(item)">
+		<transition-group tag="ul" name="list">
+			<li :key="item" class="search-item" v-for="item in searches" @click="selectItem(item)">
 				<span class="text">{{item}}</span>
 				<span class="icon">
 					<i class="icon-delete" @click.stop="deleteOne(item)"></i>
 				</span>
 			</li>
-		</ul>
+		</transition-group>
 	</div>
 </template>
 
